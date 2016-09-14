2703820542792554b29ab34f5973e325e38b6a6b
JTong <eldertong@gmail.com>
2016-09-14 11:15:36 -0400

    next steps from jonathan

--- Coinfunc.rb
+++ Coinfunc.rb
@@ -5,6 +5,10 @@
 # eg. if you have 11 cents you should return {:dime => 1, :penny => 1}; if you have 31 cents you should return {:quarter => 1, :nickel => 1, :penny => 1}.
 
 # Build up your coin_changer using tests. Commit to git after every test.
+# Next steps: 
+# 1) see if you can remove the hard coded numbers from the logic of the code (can you set variable names at the top of the function to certain values that you can use in their place)?
+# 2) make the penny if statement exactly as the others
+# 3) see if you can find a way to only have a single if statement (think iteration)
 
 def coin_sorter(cents_received)
 	coins = {}
@@ -30,6 +34,7 @@ def coin_sorter(cents_received)
 	end
 	if cents_received >= 1
 		coins[:penny] = cents_received / 1
+		cents_received = cents_received % 1
 	end
 	coins
 end
